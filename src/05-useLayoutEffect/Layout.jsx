3921595e9b06a7e302a3e226c5b2d8de389d9bf8
Antonio Mora <j_antonio_mora@hotmail.com>
2023-09-25 23:21:48 -0600

    useRef and useLayoutEffect added

---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,37 @@
+import { useCounter, useFetch } from "../hooks";
+import { LoadingQuote, Quote } from "../03-examples";
+
+export const Layout = () => {
+    
+    const { counter, increment } = useCounter(1);
+    const { data, isLoading, hasError} = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${ counter }`);
+    const { quote, author } = !!data && data[0];
+
+
+    //console.log(data);
+
+    return (
+        <>
+            <h1>Breaking bad quotes</h1>
+            <hr />
+
+            {
+                ( isLoading ) ? 
+                    <LoadingQuote /> 
+                : 
+                    <Quote
+                        quote={ quote }
+                        author= {author }
+                    />                        
+                    
+            } 
+            
+            <button 
+                className="btn btn-primary" 
+                onClick={ () => increment() }
+                disabled= { isLoading }
+            >Next Quote</button>
+
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { HooksApp } from './HooksApp';
-import { CounterApp } from './01-useState/CounterApp';
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
-import { SimpleForm } from './02-useEffect/SimpleForm';
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+// import { HooksApp } from './HooksApp';
+// import { CounterApp } from './01-useState/CounterApp';
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+// import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+// import { FocusScreen } from './04-useRef/FocusScreen';
+// import { Layout } from './05-useLayoutEffect/Layout';
+import { Memorize } from './06-memos/Memorize';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	//<React.StrictMode>
-		<MultipleCustomHooks />
+		<Memorize />
 	//</React.StrictMode>,
 	)

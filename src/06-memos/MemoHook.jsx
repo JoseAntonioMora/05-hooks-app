2c2ccae66d5af48478e95f90098eacaa5afc37a5
Antonio Mora <j_antonio_mora@hotmail.com>
2023-09-25 23:40:30 -0600

    Examples about useMemo hook

--- src/06-memos/MemoHook.jsx
+++ src/06-memos/MemoHook.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useCounter } from "../hooks"
-import { Small } from "./Small";
 
-export const Memorize = () => {
+const heavyStuff = ( iterationNumber = 100) => {
+    for( let i = 0; i< iterationNumber; i++){
+        console.log('ahi vamos');
+    }
 
-    const { counter, increment } = useCounter(10);
+    return`${ iterationNumber } iteraciones realizadas`;
+}
+
+export const MemoHook = () => {
+
+    const { counter, increment } = useCounter(4000);
     const [ show, setShow ] = useState(true);
+
+    const memorizeMemo = useMemo(() => heavyStuff(counter), [counter]);
 
     return (
         <>
-            <h1>Counter: <Small value={ counter } /></h1>
+            <h1>Counter: <small>{ counter } </small></h1>
             <hr />
+
+            <h4> { memorizeMemo } </h4>
 
             <button
                 className="btn btn-primary"

8ba4f489c27057aa99677f5cd10cf334ff2a58ac
Antonio Mora <j_antonio_mora@hotmail.com>
2023-10-03 18:22:58 -0600

    Routes added for Login, About and Main page

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,9 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { HomePage, AboutPage, LoginPage } from "./index"
 
 export const MainApp = () => {
     return (
         <>
-        <h1>Main Page</h1>
-        <hr />
+            <h1>Main Page</h1>
+            <hr />
+
+            <Routes>
+                <Route path="/" element={ <HomePage /> } />
+                <Route path="about" element={ <AboutPage /> } />
+                <Route path="login" element={ <LoginPage /> } />
+
+                {/* <Route path="/*" element={ <LoginPage /> } /> */}
+                <Route path="/*" element={ <Navigate to="/" /> } />
+            </Routes>
+
         </>    
     )
 }
